64d950ca194647a093851b81fe33b57461b6dce1
victor <104682641+Victor4430@users.noreply.github.com>
2022-07-09 08:39:41 +0800

    Add files via upload

---
+++ auto-py-to-exe/test.py
@@ -0,0 +1,11 @@
+''':cvar
+
+方法2：使用auto-py-to-exe将Python程序打包成exe（图形化方式）
+
+auto-py-to-exe 是一个用于将Python程序打包成可执行文件的图形化工具。
+安装 ：
+
+pip install auto-py-to-exe
+1
+启动：在cmd 命令中输入：auto-py-to-exe ，即可打开界面：
+'''
