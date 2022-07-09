3eab16056afa218cb3759aa7a3da1c04759aeec0
victor <104682641+Victor4430@users.noreply.github.com>
2022-07-09 09:13:57 +0800

    Add files via upload

---
+++ site_music_html/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :py_venv -> test.py
+# @IDE       :PyCharm
+# @Time      :2022/6/16 23:54
+# @Author    :杨晓东
+# @Email     :lzj155@foxmail.com
+# @homepage  :www.demo520.com
+
+import csv
+import requests
+from lxml import etree
+
+
+def run():
+    a = [1,2,1,1,2,3,3,45,6,45,8]
+    print(a.count(2))
+
+if __name__ == "__main__":
+    run()
