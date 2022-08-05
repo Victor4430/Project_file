424e69a49b20e3c2ace4f353aebc4a7091d13105
victor <104682641+Victor4430@users.noreply.github.com>
2022-08-06 02:46:07 +0800

    Update pip 一次性安装很多库.py

--- 便捷_数据库操作_批量安装库/pip 一次性安装很多库.py
+++ 便捷_数据库操作_批量安装库/pip 一次性安装很多库.py
@@ -5,8 +5,6 @@ pip一次性安装很多库
 
 2、问题解决：首先将自己所需的库放在一个requirement.txt文件中，然后通过参数-r来安装文件里所列出的所有库。示例如下：
 
-
-
 pip install -r requirement.txt
 
 1、   如果安装的比较慢，可以换安装源，需要参数-i，示例如下：
@@ -17,6 +15,104 @@ pip install -r requirement.txt
         or
     pip install -i https://mirrors.aliyun.com/pypi/simple -r requirement.txt
 
+requirement.txt
 
+asyncio
+bs4
+beautifulsoup4
+docopt
+docx
+django
+flask
+werobot
+wheel
+requests
+lxml
+xlwt
+numpy
+networkx
+sklearn
+Scikit-Learn
+TensorFlow
+Keras
+Scapy
+scrapy
+SciPy
+selenium
+sympy
+matplotlib
+multiprocessing
+threading
+jieba
+jsonpath
+pandas
+Pillow
+playwright
+pdfplumber
+pypdf2
+pyautogui
+pyperclip
+Pyglet
+pymysql
+pyqt5
+pywifi
+pyopengl
+pygame
+pyinstaller
+pymongo
 
 """
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :py_venv -> pip 一次性安装很多库.py
+# @IDE       :PyCharm
+# @Time      :2022/5/9 17:15
+# @Author    :杨晓东
+# @Email     :lzj155@foxmail.com
+# @homepage  :www.demo443.com
+
+import os
+
+
+def auto_install():
+    # print 字体紫色
+    color_a = "\033[1;35m"
+    color_b = '\033[0m'
+    # color_a +
+    # + color_b
+
+    libs = ['requests', 'asyncio', 'bs4', 'beautifulsoup4', 'docopt', 'docx', 'django', 'flask', 'werobot', 'wheel',
+            'lxml', 'xlwt',
+            'numpy', 'networkx', 'sklearn', 'Scikit-Learn', 'TensorFlow', 'Keras', 'Scapy', 'scrapy', 'SciPy',
+            'selenium', 'sympy', 'matplotlib', 'multiprocessing', 'threading', 'jieba', 'jsonpath', 'pandas', 'Pillow',
+            'playwright', 'pdfplumber',
+            'pypdf2', 'pyautogui', 'pyperclip', 'Pyglet', 'pymysql', 'pyqt5', 'pywifi', 'pyopengl', 'pygame',
+            'pyinstaller', 'pymongo']
+    # 计数器
+    num = 0
+    # 需要安装的库的数量
+    a = len(libs)
+    # 统计出错次数
+    c = 0
+    for lib in libs:
+        # 已执行数量
+        num += 1
+        # 剩余未执行数量
+        b = a - num
+
+        try:
+            print(color_a + '当前安装第 ', num, ' 个库，剩余：', b, ' 个库正在等待安装....' + color_b)
+            print(color_a + '开始安装： ', lib + color_b)
+            print(color_a + "执行命令：  pip install -i https://pypi.tuna.tsinghua.edu.cn/simple " + lib + color_b)
+            os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ' + lib)
+            print(color_a + lib + '：' + '  installed successful 已安装成功！' + color_b)
+        except:
+            # 出错次数累计
+            c += 1
+            print("\033[1;31m" + lib + '  installed failed      安装失败！' + color_b)
+
+    print(color_a + str(libs), '\n', "已成功安装", num, "个库！", c, "个异常！   \n程序退出！" + color_b)
+
+
+if __name__ == '__main__':
+    auto_install()
